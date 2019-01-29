b492a185f60212cdfe49a7ff558b2744855623d6
basterg <sitxbasterg@inbox.ru>
2019-01-29 16:07:40 +0500

    1st commit

---
+++ task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print (int n) {
+        System.out.println(n);
+    }
+
+    public static void print (short n) {
+        System.out.println(n);
+    }
+
+    public static void print (char n) {
+        System.out.println(n);
+    }
+
+    public static void print (String s) {
+        System.out.println(s);
+    }
+
+    public static void print (int n, short sh) {
+        System.out.println(n + sh);
+    }
+}
