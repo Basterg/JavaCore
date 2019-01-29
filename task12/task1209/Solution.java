5cc33afd1aa36a8aa67f679d8914d7f392c9addf
basterg <sitxbasterg@inbox.ru>
2019-01-29 16:11:59 +0500

    1st commit

--- task12/task1209/Solution.java
+++ task12/task1209/Solution.java
@@ -9,5 +9,15 @@ public class Solution {
 
     }
 
-    //Напишите тут ваши методы
+    public static int min(int a, int b) {
+        return a > b ? b : a;
+    }
+
+    public static long min(long a, long b) {
+        return a > b ? b : a;
+    }
+
+    public static double min(double a, double b) {
+        return a > b ? b : a;
+    }
 }
