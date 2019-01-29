09b091a9737d2dd924f198861a2d104b33768932
basterg <sitxbasterg@inbox.ru>
2019-01-29 16:05:34 +0500

    1st commit

---
+++ task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 1;
+        Integer integer = 5;
+        print(i);
+        print(integer);
+    }
+
+    public static void print (Integer n) {
+        System.out.println(n);
+    }
+
+    public static void print (int n) {
+        System.out.println(n);
+    }
+}
