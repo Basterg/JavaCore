c42097d0b912d5f3f00d1fac066714b7a98ed42e
basterg <sitxbasterg@inbox.ru>
2019-02-07 18:43:01 +0500

    1st commit

--- task13/task1315/Solution.java
+++ task13/task1315/Solution.java
@@ -22,4 +22,48 @@ public class Solution {
     public interface Eat {
         void eat();
     }
+
+    public static class Cat implements Movable, Edible, Eat {
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void beEaten() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
+
+    public static class Mouse implements Movable, Edible {
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void beEaten() {
+
+        }
+    }
+
+    public static class Dog implements Movable, Eat {
+
+        @Override
+        public void move() {
+
+        }
+
+        @Override
+        public void eat() {
+
+        }
+    }
 }
