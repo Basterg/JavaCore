1cdc56b46bfbea70427c919f35e645916c3e7992
basterg <sitxbasterg@inbox.ru>
2019-02-10 17:43:46 +0500

    1st commit

--- task14/task1404/Solution.java
+++ task14/task1404/Solution.java
@@ -4,9 +4,20 @@ package com.javarush.task.task14.task1404;
 Коты
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String string = reader.readLine();
+            if (string.equals("")) {
+                break;
+            }
+            Cat cat = CatFactory.getCatByKey(string);
+            System.out.println(cat.toString());
+        }
     }
 
     static class CatFactory {
