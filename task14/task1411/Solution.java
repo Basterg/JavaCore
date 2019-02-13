eb43ab803017d4f55f3b6f7abf56ac07e25f7041
basterg <sitxbasterg@inbox.ru>
2019-02-13 17:55:23 +0500

    1st commit

--- task14/task1411/Solution.java
+++ task14/task1411/Solution.java
@@ -13,16 +13,33 @@ public class Solution {
         Person person = null;
         String key = null;
 
-        //тут цикл по чтению ключей, пункт 1
-        {
-            //создаем объект, пункт 2
+        while (true) {
+            key = reader.readLine();
+            if (key.equals("user")) {
+                person = new Person.User();
+            } else if (key.equals("loser")) {
+                person = new Person.Loser();
+            } else if (key.equals("coder")) {
+                person = new Person.Coder();
+            } else if (key.equals("proger")) {
+                person = new Person.Proger();
+            } else {
+                break;
+            }
 
-            doWork(person); //вызываем doWork
-
+            doWork(person);
         }
     }
 
     public static void doWork(Person person) {
-        // пункт 3
+        if (person instanceof Person.User) {
+            ((Person.User) person).live();
+        } else if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        } else if (person instanceof Person.Coder) {
+            ((Person.Coder) person).writeCode();
+        } else {
+            ((Person.Proger) person).enjoy();
+        }
     }
 }
