51c5350003af96bb82b7afb83120f47013290e8a
basterg <sitxbasterg@inbox.ru>
2019-03-15 18:48:28 +0500

    1st commit

--- task16/task1629/Solution.java
+++ task16/task1629/Solution.java
@@ -10,12 +10,32 @@ public class Solution {
     public static void main(String[] args) throws InterruptedException {
         Read3Strings t1 = new Read3Strings();
         Read3Strings t2 = new Read3Strings();
-
-        //add your code here - добавьте код тут
-
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
         t1.printResult();
         t2.printResult();
     }
 
-    //add your code here - добавьте код тут
+    public static class Read3Strings extends Thread {
+        public String result = "";
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    String string = reader.readLine();
+                    result += string + " ";
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void  printResult() {
+            System.out.println(result);
+        }
+    }
 }
