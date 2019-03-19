ae24b213b1b247d8cbb89335a6c3ea2889b9b31b
basterg <sitxbasterg@inbox.ru>
2019-03-19 17:17:39 +0500

    1st commit

--- task17/task1706/OurPresident.java
+++ task17/task1706/OurPresident.java
@@ -2,7 +2,11 @@ package com.javarush.task.task17.task1706;
 
 public class OurPresident {
     private static OurPresident president;
-
+    static {
+        synchronized (Solution.class){
+            president = new OurPresident();
+        }
+    }
     private OurPresident() {
     }
 
