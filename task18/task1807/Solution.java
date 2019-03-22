4fe7b564fc1dc2d5acdb0f974b3c654041e6cc92
basterg <sitxbasterg@inbox.ru>
2019-03-22 17:57:24 +0500

    1st commit

--- task18/task1807/Solution.java
+++ task18/task1807/Solution.java
@@ -4,7 +4,27 @@ package com.javarush.task.task18.task1807;
 Подсчет запятых
 */
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+        int count = 0;
+        while (inputStream.available() > 0) {
+            int symbolCode = inputStream.read();
+            if (symbolCode == 44) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
+
+
 }
