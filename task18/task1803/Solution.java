f86b8f6e9542d26f01151577aedc01d3e11837f1
basterg <sitxbasterg@inbox.ru>
2019-03-22 16:59:50 +0500

    2nd commit

--- task18/task1803/Solution.java
+++ task18/task1803/Solution.java
@@ -2,53 +2,73 @@ package com.javarush.task.task18.task1803;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /* 
 Самые частые байты
 */
 
 public class Solution {
-    static HashMap<Integer, Integer> mapOfBytes = new HashMap<>();
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static HashMap<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String filename = reader.readLine();
-
-        FileInputStream inputStream = new FileInputStream(filename);
-
-        int value = 0;
-        Integer count = 0;
-        while (inputStream.available() > 0) {
-            value = inputStream.read();
-            count = mapOfBytes.get(value);
-            if (count != null)
-                count++;
-            else
-                count = 0;
-
-            mapOfBytes.put(value, count);
-        }
-
-        inputStream.close();
-
-        boolean first = true;
-        int max = 0;
-        for (int amountByte : mapOfBytes.values()) {
-            if (first) {
-                max = amountByte;
-                first = false;
-            }
-            if (max < amountByte)
-                max = amountByte;
-        }
-
-        for (Map.Entry pair : mapOfBytes.entrySet()) {
-            if (max == (int) pair.getValue())
-                System.out.print(" " + pair.getKey());
-        }
-
+        fillTheListBytes();
+        fillTheMapBytesAndCounts();
+        printMaxRepeatedBytes();
     }
+
+    public static String readFileName() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        return fileName;
+    }
+
+    public static void fillTheListBytes() throws IOException {
+        FileInputStream inputStream = new FileInputStream(readFileName());
+        int currentByte;
+        while (inputStream.available() > 0) {
+            currentByte = inputStream.read();
+            list.add(currentByte);
+        }
+        inputStream.close();
+    }
+
+    public static void fillTheMapBytesAndCounts() {
+        for (int itrByte : list) {
+            int newValue;
+            if (map.containsKey(itrByte)) {
+                newValue = map.get(itrByte) + 1;
+                map.put(itrByte, newValue);
+            } else {
+                map.put(itrByte, 1);
+            }
+        }
+    }
+
+    public static int findMaxCount() {
+        ArrayList<Integer> listOfCounts = new ArrayList<>(map.values());
+        int max = 0;
+        for (int maxCount : listOfCounts) {
+            if (maxCount > max) {
+                max = maxCount;
+            }
+        }
+        return max;
+    }
+
+    public static void printMaxRepeatedBytes() {
+        int max = findMaxCount();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (value == max) {
+                System.out.print(key + " ");
+            }
+        }
+    }
+
 }
