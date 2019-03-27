d8aaca1b45436acea7da3791eecc2f393b8d27d6
basterg <sitxbasterg@inbox.ru>
2019-03-27 14:20:55 +0500

    1st commit

--- task18/task1817/Solution.java
+++ task18/task1817/Solution.java
@@ -4,8 +4,25 @@ package com.javarush.task.task18.task1817;
 Пробелы
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
 
+public class Solution {
+    public static int count = 0;
+    public static int spacesCount = 0;
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("C:/data1.txt");
+        int cuurentByte;
+        while (fileInputStream.available() > 0) {
+            cuurentByte = fileInputStream.read();
+            if (cuurentByte == 32) {
+                spacesCount++;
+            } else {
+                count++;
+            }
+        }
+        fileInputStream.close();
+
+        System.out.println(String.format("%.2f", ((float) spacesCount / count) * 100));
     }
 }
