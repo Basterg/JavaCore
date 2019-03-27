62de33d7ce7f708795ad79168a680bb8e5891e00
basterg <sitxbasterg@inbox.ru>
2019-03-27 14:06:06 +0500

    1st commit

--- task18/task1816/Solution.java
+++ task18/task1816/Solution.java
@@ -4,8 +4,20 @@ package com.javarush.task.task18.task1816;
 Английские буквы
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class Solution {
+    public static int count = 0;
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("C:/data1.txt");
+        while (fileInputStream.available() > 0) {
+            int currentByte = fileInputStream.read();
+            if ((currentByte >= 64 && currentByte <= 90) || (currentByte >= 97 && currentByte <= 122)) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
