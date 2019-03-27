6b7e00aa3877e0fdb4c01abd91847b9acc1113c3
basterg <sitxbasterg@inbox.ru>
2019-03-27 15:58:53 +0500

    1st commit

--- task18/task1821/Solution.java
+++ task18/task1821/Solution.java
@@ -4,8 +4,25 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+public class Solution {
+    public static int[] bytes = new int[256];
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("C:/data1.txt");
+        int currentByte;
+        char currentChar;
+        while (fileInputStream.available() > 0) {
+            currentByte = fileInputStream.read();
+            bytes[currentByte] += 1;
+        }
+        fileInputStream.close();
+
+        for (int i = 0; i < 256; i++) {
+            currentChar = (char) i;
+            System.out.println(currentChar + " " + bytes[i]);
+        }
     }
 }
