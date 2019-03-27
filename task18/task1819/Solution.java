93b5d5982420cc079dbff1a3a9bb0b58346606b1
basterg <sitxbasterg@inbox.ru>
2019-03-27 15:20:35 +0500

    1st commit

--- task18/task1819/Solution.java
+++ task18/task1819/Solution.java
@@ -4,8 +4,30 @@ package com.javarush.task.task18.task1819;
 Объединение файлов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        reader.close();
+
+        FileInputStream fileInputStream1 = new FileInputStream(fileName1);
+        FileInputStream fileInputStream2 = new FileInputStream(fileName2);
+
+        byte[] buffer1 = new byte[fileInputStream1.available()];
+        fileInputStream1.read(buffer1);
+        fileInputStream1.close();
+
+        byte[] buffer2 = new byte[fileInputStream2.available()];
+        fileInputStream1.read(buffer2);
+        fileInputStream1.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName1);
+        fileOutputStream.write(buffer2);
+        fileOutputStream = new FileOutputStream(fileName1, true);
+        fileOutputStream.write(buffer1);
+        fileOutputStream.close();
     }
 }
