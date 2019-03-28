02f0716279fae10570fa558b0922abe41e09bbb6
basterg <sitxbasterg@inbox.ru>
2019-03-28 14:28:08 +0500

    1st commit

--- task18/task1823/Solution.java
+++ task18/task1823/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task18.task1823;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,14 +11,51 @@ import java.util.Map;
 public class Solution {
     public static Map<String, Integer> resultMap = new HashMap<>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        String fileName;
+        while (true) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            fileName = reader.readLine();
+            if (fileName.equals("exit")) {
+                reader.close();
+                break;
+            }
+            ReadThread readThread = new ReadThread(fileName);
+            readThread.start();
+        }
     }
 
     public static class ReadThread extends Thread {
+        String fileName;
+
         public ReadThread(String fileName) {
-            //implement constructor body
+            this.fileName = fileName;
         }
-        // implement file reading here - реализуйте чтение из файла тут
+
+        @Override
+        public void run() {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(fileName);
+                while (fileInputStream.available() > 0) {
+                    byte[] buf = new byte[fileInputStream.available()];
+                    fileInputStream.read(buf);
+                    resultMap.put(fileName, findMaxByte(buf));
+                }
+                fileInputStream.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static int findMaxByte(byte[] bytes) {
+        int max = 0;
+        for (int i = 0; i < bytes.length; i++) {
+            if (bytes[i] > max) {
+                max = bytes[i];
+            }
+        }
+        return max;
     }
 }
