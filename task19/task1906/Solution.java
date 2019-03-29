56a2c252260672299a458fb5c6b60b1168c77cfe
basterg <sitxbasterg@inbox.ru>
2019-03-29 18:43:07 +0500

    1st commit

--- task19/task1906/Solution.java
+++ task19/task1906/Solution.java
@@ -4,8 +4,26 @@ package com.javarush.task.task19.task1906;
 Четные символы
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String firstFileName = reader.readLine();
+        String secondFileName = reader.readLine();
+        reader.close();
+
+        FileReader fileReader = new FileReader(firstFileName);
+        FileWriter fileWriter = new FileWriter(secondFileName);
+
+        int count = 1;
+        while (fileReader.ready()) {
+            int value = fileReader.read();
+            if (count % 2 == 0)
+                fileWriter.write(value);
+            count++;
+        }
+        fileReader.close();
+        fileWriter.close();
     }
 }
